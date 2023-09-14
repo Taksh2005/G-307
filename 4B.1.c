4cd87e2c39efdd698ff0be836c2d8f2765069cc1
Taksh2005 <144982806+Taksh2005@users.noreply.github.com>
2023-09-14 10:20:27 +0530

    Add files via upload

---
+++ 4B.1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+void main(){
+	float h,b;
+	printf("Enter Hight:");
+	scanf("%f",&h);
+	printf("Enter Base length:");
+	scanf("%f",&b);
+	printf("area of triangle = %f unit sq.",h*b*1/2);
+}
