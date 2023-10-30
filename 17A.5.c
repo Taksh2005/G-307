4523751fd1581ec9423009903cda30ae5f94b553
Taksh2005 <144982806+Taksh2005@users.noreply.github.com>
2023-10-30 09:19:19 +0530

    Update 17A.5.c

--- 17A.5.c
+++ 17A.5.c
@@ -1,16 +1,17 @@
-//Store n elements in an array and print the elements using pointer.
+//store n element in an array and print the elements using pointer.
 #include<stdio.h>
 void main(){
-	int n,i;
-	printf("enter no. of element for array: ");
+	int n;
+	printf("enter number of element for array: ");
 	scanf("%d",&n);
-	int a[n],*p[n];
+	int a[n],*p,i;
 	for(i=0;i<n;i++){
-		printf("enter value in a[%d]: ",i);
+		printf("Enter value in array[%d]: ",i);
 		scanf("%d",&a[i]);
-		p[i]=&a[i];
 	}
+	p=a;
 	for(i=0;i<n;i++){
-		printf("%d\n",*p[i]);
+		printf("%d",*(p+i));
+		printf("\n");
 	}
 }
